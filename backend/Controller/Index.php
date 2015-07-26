3564fe40ca034aa340538f05e04d428f9fc96b21
follower <vitfollower@gmail.com>
2015-07-26 22:18:34 +0300

    new template

--- backend/Controller/Index.php
+++ backend/Controller/Index.php
@@ -6,6 +6,6 @@ class Index extends \Core\Controller
 {
 	public function methodIndex($args)
 	{
-		return $this->view->render($this->_appPath . '/vendor/follower/admin/templates/base.phtml', []);
+		return $this->view->render('../vendor/follower/admin/public/templates/base.phtml', []);
 	}
 }
