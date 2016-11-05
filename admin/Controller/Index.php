aafa31f186ee283eb33195c507a569f48e245ef4
Vitaliy Malyshev <vmalyshev@rocketfuel.com>
2016-11-06 00:03:23 +0200

    remove unnecessary from admin tpl

--- admin/Controller/Index.php
+++ admin/Controller/Index.php
@@ -7,18 +7,6 @@ class Index extends Controller
 	public function methodIndex($args)
 	{
 		$data['content'] = $this->view->render('templates/index.phtml', $args);
-
-		$this->addCssPath([
-			'/bower_components/morrisjs/morris.css',
-			'/dist/css/timeline.css',
-		]);
-
-		$this->addJavaScriptPath([
-			'/bower_components/raphael/raphael-min.js',
-			'/bower_components/morrisjs/morris.min.js',
-			'/js/morris-data.js',
-		]);
-
 		return $this->render($data);
 	}
 }
