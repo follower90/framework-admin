b4ad80058a03c9e1551c3c29997d0f58590373f9
Vitaliy Malyshev <vmalyshev@rocketfuel.com>
2016-11-30 17:51:46 +0200

    fix logout

--- admin/Controller/Logout.php
+++ admin/Controller/Logout.php
@@ -11,7 +11,7 @@ class Logout extends Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->_authorize = new \Core\Authorize('Admin');
+		$this->_authorize = new \Admin\Authorize('Admin');
 	}
 
 	public function methodIndex()
