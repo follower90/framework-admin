f828278892768a0b7b5f070f11ff49776a01510d
Vitaliy Malyshev <vmalyshev@rocketfuel.com>
2017-09-05 16:38:25 +0300

    image resizer

--- app/Api/Resource.php
+++ app/Api/Resource.php
@@ -18,4 +18,25 @@ class Resource extends \Core\Api
 
 		\Core\Library\File::upload($src, $resource->getValue('name'));
 	}
+
+	public function methodGetImageResized($args)
+	{
+		if (!$resource = \Admin\Object\Resource::find($args['id'])) {
+			throw new \Core\Exception\Exception('Resource not found');
+		}
+
+		if (!$args['width'] || !$args['height']) {
+			throw new \Core\Exception\Exception('You need to specify width and height');
+		}
+
+		$view = new \Core\View();
+		$src = $resource->getPath();
+
+		if (!file_exists($src)) {
+			throw new \Core\Exception\Exception('File does not exist on storage');
+		}
+
+		$src = $view->imageResize($src, $args['width'], $args['height']);
+		\Core\Library\File::upload($src, $resource->getValue('name'));
+	}
 }
